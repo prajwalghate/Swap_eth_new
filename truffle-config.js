379d59964183afb5171842dc47f26331eca802b3
Linux <prajwalghate18.com>
2020-05-12 00:54:17 +0530

    First Commit

---
+++ truffle-config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+/*
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_directory: './contracts/',
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+  }
+};
+
+*/
+
+
+
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+  },
+  contracts_directory: './contracts/',
+  contracts_build_directory: path.join(__dirname, "/src/contracts"),
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "petersburg"
+    }
+  }
+}
